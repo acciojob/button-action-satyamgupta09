ded5c36544ab42d39305169f870e07a85ae4ae4e
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-01-26 04:43:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,24 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
+  const [show, setShow] = useState(false);
 
+  function handleShow() {
+    setShow((prevState) => !prevState);
+  }
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      {show ? (
+        <p className={show ? "show" : "hide"}>
+          "Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy"
+        </p>
+      ) : null}
+      <button id="click" onClick={handleShow}>
+        Click
+      </button>
     </div>
   );
-}
+};
 
-export default App
+export default App;
